f2fee83e743ae7c39957d5b10a8fe5502f8676fc
andbamp <andreasbampouris@gmail.com>
2022-02-11 14:37:05 +0000

    Add test for preprocessing function

--- tests/testthat/test-eqf_embeddings.R
+++ tests/testthat/test-eqf_embeddings.R
@@ -4,3 +4,9 @@ test_that("dimensions of embeddings are consistent", {
   res <- dim(eqf_embeddings(x, "en"))
   expect_equal(res, exp)
 })
+
+test_that("embeddings are identical for texts that become identical through preprocessing", {
+  x <- c("High School", "High School!", "high  school\t", "High\t\tschool ...")
+  emb <- eqf_embeddings(x)
+  expect_true(all(emb[1, ] == t(emb)))
+})
