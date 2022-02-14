eafee836bd1201ef1502058897abe3b43236d0d8
andbamp <andreasbampouris@gmail.com>
2022-02-14 12:26:16 +0200

    Update test for 25-dims LSA space

--- tests/testthat/test-eqf_embeddings.R
+++ tests/testthat/test-eqf_embeddings.R
@@ -1,6 +1,6 @@
 test_that("dimensions of embeddings are consistent", {
   x <- c("Master of Science", "PhD in Linguistics")
-  exp <- c(length(x), 10)
+  exp <- c(length(x), 25)
   res <- dim(eqf_embeddings(x, "en"))
   expect_equal(res, exp)
 })
