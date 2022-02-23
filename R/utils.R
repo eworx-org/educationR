c79f0b2a3da2c0667d151123c4153bd1bac7f881
andbamp <andreasbampouris@gmail.com>
2022-02-23 20:01:43 +0200

    Add max number of characters per word on preprocessing function

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,18 @@
 #' Preprocess character vectors
 #' 
 #' @param x A character vector.
+#' @param num_max_char An integer equal to the maximum number of characters per word desired, unless set to `NULL`.
 #' 
-#' @return Character vector with lower case alphanumeric text and trimmed white space.
-prep_fun <- function(x) {
+#' @return Character vector with lower case alphanumeric text, trimmed white space, and maximum number of characters per word.
+prep_fun <- function(x, num_max_char = 10L) {
   x <- tolower(x)
-  x <- gsub("\t", " ", x)
-  x <- gsub("[^[:alnum:][:space:]]", " ", x)
-  trimws(x)
+  x <- gsub("[^[:alnum:] ]", " ", x)
+  x <- gsub("[[:space:]]+", " ", x)
+  if(!is.null(num_max_char)) {
+    pattern <- paste0("(^|\\W)\\w{1,", num_max_char, "}")
+    x <- sapply(regmatches(x, gregexpr(pattern, x)), paste, collapse = "")
+  } else {
+    x <- trimws(x)
+  }
+  x
 }
