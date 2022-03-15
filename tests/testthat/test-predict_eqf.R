1eaddb57671f99bf19fea848573925bec8414020
andbamp <andreasbampouris@gmail.com>
2022-03-15 23:29:19 +0200

    Add NA threshold on EQF prediction

--- tests/testthat/test-predict_eqf.R
+++ tests/testthat/test-predict_eqf.R
@@ -11,3 +11,17 @@ test_that("predictions for some typical qualifications are correct", {
   res <- predict_eqf(x)
   expect_equal(res, exp)
 })
+
+test_that("output is a matrix if na_threshold is NULL", {
+  x <- c("Master of Science", "PhD in Linguistics")
+  exp <- c("matrix", "array")
+  res <- class(predict_eqf(x, na_threshold = NULL))
+  expect_equal(res, exp)
+})
+
+test_that("output length is 8 * input length if na_threshold is NULL", {
+  x <- c("Master of Science", "PhD in Linguistics")
+  exp <- 8 * length(x)
+  res <- length(predict_eqf(x, na_threshold = NULL))
+  expect_equal(res, exp)
+})
