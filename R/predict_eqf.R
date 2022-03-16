2c4c5189282564568ea01f8da23ed0ddcad0fbcb
andbamp <andreasbampouris@gmail.com>
2022-03-16 12:33:44 +0200

    Make prediction based on all languages

--- R/predict_eqf.R
+++ R/predict_eqf.R
@@ -11,14 +11,24 @@
 #' @examples
 #' predict_eqf("Bachelor of Science")
 #' predict_eqf(c("Master of Science", "PhD in Linguistics"), "en")
-predict_eqf <- function(x, locale = "en", na_threshold = 0.25) {
-  # Extract features
-  x <- itoken(x, preprocessor = prep_fun, progressbar = FALSE)
-  x <- create_dtm(x, models$eqf[[locale]][["vec"]])
-  x <- transform(x, models$eqf[[locale]][["tfidf"]])
-  x <- transform(x, models$eqf[[locale]][["lsa"]])
-  # Calculate score per EQF level and return if scores is TRUE
-  pred <- predict(models$eqf[[locale]][["model"]], x, type = "response")[, , 1]
+predict_eqf <- function(x, locale = NULL, na_threshold = 0.25) {
+  # Calculate prediction for every supported language if locale is NULL
+  if(is.null(locale)) {
+    pred <- 0
+    for(loc in get_languages_eqf()) {
+      pred <- pred + predict_eqf(x, loc, na_threshold = NULL)
+    }
+    pred <- pred / length(get_languages_eqf())
+  }
+  else {
+    # Extract features
+    x <- itoken(x, preprocessor = prep_fun, progressbar = FALSE)
+    x <- create_dtm(x, models$eqf[[locale]][["vec"]])
+    x <- transform(x, models$eqf[[locale]][["tfidf"]])
+    x <- transform(x, models$eqf[[locale]][["lsa"]])
+    # Calculate score per EQF level and return if scores is TRUE
+    pred <- predict(models$eqf[[locale]][["model"]], x, type = "response")[, , 1]
+  }
   if(is.null(na_threshold)) {
     return(pred)
   }
