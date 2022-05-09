b9edbef07ef3e23ad87e86a1806d8fa29d04fb19
andbamp <andreasbampouris@gmail.com>
2022-05-09 17:37:08 +0300

    Refactor EQF function

--- R/predict_eqf.R
+++ R/predict_eqf.R
@@ -1,9 +1,8 @@
-#' Predict the EQF level of a qualification title
+#' Classify qualification titles with respect to EQF level
 #' 
 #' @param x A character vector of qualification titles.
-#' @param locale The ISO 639-1 code of the language used.
-#' @param na_threshold Threshold for EQF level prediction score.
-#' @return If `na_threshold` is not `NULL`, a factor of equal length to x with the predicted EQF levels. Otherwise, a matrix with as many rows as length of x and scores for each EQF level.
+#' @param locale The ISO 639-1 code of the language used. If a character vector with multiple codes is provided, the final prediction will be the average of predictions across those languages.
+#' @return A matrix with as many rows as length of x and scores for each EQF level.
 #' @import text2vec
 #' @import glmnet
 #' @export
@@ -11,34 +10,46 @@
 #' @examples
 #' predict_eqf("Bachelor of Science")
 #' predict_eqf(c("Master of Science", "PhD in Linguistics"), "en")
-predict_eqf <- function(x, locale = NULL, na_threshold = 0.25) {
+predict_eqf <- function(x, locale = get_languages_eqf()) {
   # Calculate prediction for every supported language if locale is NULL
-  if(is.null(locale)) {
-    pred <- 0
-    for(loc in get_languages_eqf()) {
-      pred <- pred + predict_eqf(x, loc, na_threshold = NULL)
-    }
-    pred <- pred / length(get_languages_eqf())
+  if(length(locale) > 1) {
+    pred <- lapply(locale, function(loc) {
+      predict_eqf(x, loc)
+    })
+    pred <- apply(simplify2array(pred), c(1,2), sum, na.rm = TRUE)
+    pred[apply(pred, 1, sum) == 0] <- NA
+    pred <- pred / apply(pred, 1, sum)
   }
   else {
     # Extract features
-    x <- itoken(x, preprocessor = prep_fun, progressbar = FALSE)
-    x <- create_dtm(x, models$eqf[[locale]][["vec"]])
-    x <- transform(x, models$eqf[[locale]][["tfidf"]])
-    x <- transform(x, models$eqf[[locale]][["lsa"]])
-    # Calculate score per EQF level and return if scores is TRUE
-    pred <- predict(models$eqf[[locale]][["model"]], x, type = "response")[, , 1]
+    y <- itoken(x, preprocessor = prep_fun, progressbar = FALSE)
+    y <- create_dtm(y, models$eqf[[locale]][["vec"]])
+    no_match <- apply(y == 0, 1, all)
+    y <- transform(y, models$eqf[[locale]][["tfidf"]])
+    y <- transform(y, models$eqf[[locale]][["lsa"]])
+    # Calculate score per EQF level
+    pred <- predict(models$eqf[[locale]][["model"]], y, type = "response")
+    pred <- matrix(pred, nrow = length(x))
+    pred[no_match] <- NA
   }
-  if(is.null(na_threshold)) {
-    return(pred)
-  }
+  pred
+}
+
+#' Predict the EQF level of a qualification title
+#' 
+#' @param x A character vector of qualification titles.
+#' @param locale The ISO 639-1 code of the language used. If a character vector with multiple codes is provided, the final prediction will be based on the average of predictions across those languages.
+#' @param na_threshold Threshold for EQF level prediction score.
+#' @return An integer vector equal in length to x.
+#' @export
+#' 
+#' @examples
+#' eqf_level("Bachelor of Science")
+#' eqf_level(c("Master of Science", "PhD in Linguistics"), "en")
+eqf_level <- function(x, locale = get_languages_eqf(), na_threshold = 0.25) {
   # Return the EQF level with the maximum score per qualification title.
-  if(is.matrix(pred)) {
-    eqf_pred <- apply(pred, 1, function(y) names(sort(-y[y > na_threshold]))[1])
-    eqf_levels <- colnames(pred)
-  } else {
-    eqf_pred <- names(sort(-pred[pred > na_threshold]))[1]
-    eqf_levels <- names(pred)
-  }
-  factor(unname(eqf_pred), levels = eqf_levels)
+  pred <- predict_eqf(x, locale)
+  above_threshold <- apply(pred, 1, max) >= na_threshold
+  pred <- max.col(pred)
+  ifelse(above_threshold, pred, NA)
 }
--- tests/testthat/test-predict_eqf.R
+++ tests/testthat/test-predict_eqf.R
@@ -1,27 +1,27 @@
 test_that("output length is the same as input length", {
   x <- c("Master of Science", "PhD in Linguistics")
   exp <- length(x)
-  res <- length(predict_eqf(x))
+  res <- length(eqf_level(x))
   expect_equal(res, exp)
 })
 
 test_that("predictions for some typical qualifications are correct", {
   x <- c("Bachelor's degree", "Master's degree", "PhD", "gibberish123asdf")
-  exp <- factor(c("6", "7", "8", NA), level = as.character(1:8))
-  res <- predict_eqf(x)
+  exp <- c(6, 7, 8, NA)
+  res <- eqf_level(x)
   expect_equal(res, exp)
 })
 
 test_that("output is a matrix if na_threshold is NULL", {
   x <- c("Master of Science", "PhD in Linguistics")
   exp <- c("matrix", "array")
-  res <- class(predict_eqf(x, na_threshold = NULL))
+  res <- class(predict_eqf(x))
   expect_equal(res, exp)
 })
 
 test_that("output length is 8 * input length if na_threshold is NULL", {
   x <- c("Master of Science", "PhD in Linguistics")
   exp <- 8 * length(x)
-  res <- length(predict_eqf(x, na_threshold = NULL))
+  res <- length(predict_eqf(x))
   expect_equal(res, exp)
 })
